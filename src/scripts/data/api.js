4fdd9fb70e985324555020a36043e1a05b7f06cb
Christ1312 <caes7777@gmail.com>
2025-05-03 16:30:07 +0700

    Update api.js

--- src/scripts/data/api.js
+++ src/scripts/data/api.js
@@ -7,6 +7,20 @@ const ENDPOINTS = {
   LOGIN: `${BASE_URL}/login`,
 
   // Add new stories
+  static async getNotes() {
+        return fetch(`${BASE_URL}/stories`)
+            .then((response) => {
+                if (response.status >= 200 && response.status < 300) {
+                    return response.json();
+                } else {
+                    return Promise.reject(new Error('Something went wrong'))
+                }
+            })
+            .then((responseJson) => {
+                const { data } = responseJson;
+                return data;
+            })
+    }
 
   // Add new stories (guest)
 
