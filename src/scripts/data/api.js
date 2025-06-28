148710f46f28bee8fce851ce60009da8ad76d2c4
Christ1312 <caes7777@gmail.com>
2025-06-28 15:59:52 +0700

    Add files via upload

---
+++ src/scripts/data/api.js
@@ -0,0 +1,129 @@
+import { getAccessToken } from '../utils/auth';
+import { BASE_URL } from '../config';
+
+// Get all stories
+export async function getStory() {
+  const response = await fetch(`${BASE_URL}/stories`, {
+    headers: {
+      Authorization: 'Bearer ' + getAccessToken()
+    }
+  });
+  const responseJSON = await response.json();
+  
+  return {
+    ...responseJSON,
+    ok: response.ok,
+  };
+}
+
+// Add new stories 
+export async function insertStory({ description, photo, lat, lon }) {
+  const accessToken = getAccessToken();
+  const formData = new FormData();
+  formData.set('description', description);
+  formData.append('photo', photo);
+  formData.set('lat', lat);
+  formData.set('lon', lon);
+  const fetchResponse = await fetch(ENDPOINTS.ADD_STORIES, {
+    method: 'POST',
+    headers: { Authorization: `Bearer ${accessToken}` },
+    body: formData,
+  });
+  const json = await fetchResponse.json();
+  return {
+    ...json,
+    ok: fetchResponse.ok,
+  };
+}
+
+  // Add new stories (guest)
+
+  // Detail story
+
+const ENDPOINTS = {
+  // Auth
+  REGISTER: `${BASE_URL}/register`,
+  LOGIN: `${BASE_URL}/login`,
+  ADD_STORIES: `{BASE_URL}/stories`,
+
+  // Push notifications (web push) (Story notif, Subscribe, Unsubscribe)
+  SUBSCRIBE: `${BASE_URL}/notifications/subscribe`,
+  UNSUBSCRIBE: `${BASE_URL}/notifications/subscribe`,
+};
+
+export async function getRegistered({ name, email, password }) {
+  const data = JSON.stringify({ name, email, password });
+
+  const fetchResponse = await fetch(ENDPOINTS.REGISTER, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: data,
+  });
+  const json = await fetchResponse.json();
+
+  return {
+    ...json,
+    ok: fetchResponse.ok,
+  };
+}
+
+export async function getLogin({ email, password }) {
+  const data = JSON.stringify({ email, password });
+
+  const fetchResponse = await fetch(ENDPOINTS.LOGIN, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: data,
+  });
+  const json = await fetchResponse.json();
+
+  return {
+    ...json,
+    ok: fetchResponse.ok,
+  };
+}
+
+export async function subscribePushNotification({ endpoint, keys: { p256dh, auth } }) {
+  const accessToken = getAccessToken();
+  const data = JSON.stringify({
+    endpoint,
+    keys: { p256dh, auth },
+  });
+
+  const fetchResponse = await fetch(ENDPOINTS.SUBSCRIBE, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    },
+    body: data,
+  });
+  const json = await fetchResponse.json();
+
+  return {
+    ...json,
+    ok: fetchResponse.ok,
+  };
+}
+
+export async function unsubscribePushNotification({ endpoint }) {
+  const accessToken = getAccessToken();
+  const data = JSON.stringify({
+    endpoint,
+  });
+
+  const fetchResponse = await fetch(ENDPOINTS.UNSUBSCRIBE, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    },
+    body: data,
+  });
+  const json = await fetchResponse.json();
+
+  return {
+    ...json,
+    ok: fetchResponse.ok,
+  };
+}
